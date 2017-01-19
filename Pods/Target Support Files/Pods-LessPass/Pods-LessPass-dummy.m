a49c77a19b179fd94a715e993391b9fc7376c8b9
Данила Слупский <slupdo09@gmail.com>
2017-01-19 08:47:43 +0000

    Deleted /Pods

--- Pods/Target Support Files/Pods-LessPass/Pods-LessPass-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_LessPass : NSObject
-@end
-@implementation PodsDummy_Pods_LessPass
-@end
--- Pods/Target Support Files/Pods-LessPass/Pods-LessPass-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double Pods_LessPassVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_LessPassVersionString[];
-
